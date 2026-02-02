80bd3df8004aad4e2f9ad82ff1c74a1fd7f2d439
DESKTOP-GT9KPSK\user <san9901ho@gmail.com>
2026-02-02 13:58:03 +0900

    docs: vanilla.ts 파일의 Mutator 분석 추가

--- zustand/src/vanilla.ts
+++ zustand/src/vanilla.ts
@@ -15,17 +15,28 @@ export interface StoreApi<T> {
   subscribe: (listener: (state: T, prevState: T) => void) => () => void
 }
 
+// store처럼 보이는 객체로부터 상태(state) 타입을 추출하는 역할
 export type ExtractState<S> = S extends { getState: () => infer T } ? T : never
 
+// 추출된 상태(또는 객체 타입)에서 특정 key에 접근 가능한지 판단하고, 그 결과를 타입으로 반환
 type Get<T, K, F> = K extends keyof T ? T[K] : F
 
+// mutator들의 순서 있는 기록(Ms)을 따라가며,
+// store 타입 S에 mutator를 하나씩 적용해 최종 store 타입을 계산하는 타입 레벨 실행 엔진
 export type Mutate<S, Ms> = number extends Ms['length' & keyof Ms]
-  ? S
-  : Ms extends []
-    ? S
-    : Ms extends [[infer Mi, infer Ma], ...infer Mrs]
+  ? S  // Ms는 배열
+  : Ms extends [] // Ms는 튜플(재귀 종료 조건)
+    ? S // Ms가 비어있다면 S 반환(재귀 종료)
+    : Ms extends [[infer Mi, infer Ma], ...infer Mrs] // Ms에 아직 요소가 남아있다면
       ? Mutate<StoreMutators<S, Ma>[Mi & StoreMutatorIdentifier], Mrs>
       : never
+
+// 각 middleware가 “이 mutator를 적용하면 store 타입이 이렇게 변한다”를 등록하는 
+// mutator → 결과 store 타입 매핑용 공개 확장 슬롯
+export interface StoreMutators<S, A> {}
+
+// 현재 타입 시스템에 등록된 모든 mutator의 이름(key)만 모아둔 공식 mutator 식별자 집합
+export type StoreMutatorIdentifier = keyof StoreMutators<unknown, unknown>
 
 export type StateCreator<
   T,
@@ -38,9 +49,6 @@ export type StateCreator<
   store: Mutate<StoreApi<T>, Mis>,
 ) => U) & { $$storeMutators?: Mos }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-empty-object-type
-export interface StoreMutators<S, A> {}
-export type StoreMutatorIdentifier = keyof StoreMutators<unknown, unknown>
 
 type CreateStore = {
   <T, Mos extends [StoreMutatorIdentifier, unknown][] = []>(
